d3403455b513531c92780efce950fe4999622e04
user <user@DESKTOP-M1237OQ>
2022-12-03 14:09:43 +0530

    1st commit by ayesha--project created

---
+++ JenkinDemo/src/jenkinsdemo/demoClass.java
@@ -0,0 +1,15 @@
+package jenkinsdemo;
+
+public class demoClass {
+	 static int a;
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+		System.out.println("Default value of a : " + a);
+
+		
+		System.out.println("Now I am in Git repo");
+	}
+
+}
